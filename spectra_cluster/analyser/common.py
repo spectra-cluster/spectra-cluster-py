462c3ef430335ea45fa95d586f83b158870a0a1f
Johannes Griss <jgriss@users.noreply.github.com>
2016-10-26 22:40:57 +0200

    Added sphinx based documentation

--- spectra_cluster/analyser/common.py
+++ spectra_cluster/analyser/common.py
@@ -9,6 +9,21 @@ class AbstractAnalyser:
     This class mainly provides helper functions for
     filtering clusters based on size, ratio, identified
     spectra, etc.
+
+    Additionally, every child class must implement the
+    **process_cluster(Cluster)** function.
+
+    Its init functions sets the following member variables
+    so that all clusters are accepted.
+
+    :ivar min_size: Minimal size of the cluster
+    :ivar max_size: Maximum size of the cluster
+    :ivar min_ratio: Minimum I/L agnostic ratio
+    :ivar max_ratio: Maximum I/L agnostic ratio
+    :ivar min_identified_spectra: Minimal identified spectra
+    :ivar max_identified_spectra: Maximum identified spectra
+    :ivar min_unidentified_spectra: Minimal unidentified spectra
+    :ivar max_unidentified_spectra: Maximum unidentified spectra
     """
     def __init__(self):
         """Initialises the default parameters to filter clusters.
@@ -25,6 +40,11 @@ class AbstractAnalyser:
 
     def _ignore_cluster(self, cluster):
         """Tests whether the passed cluster should be ignored
+
+        This function assess the class' instance variables
+        (min size, max size, etc.) to test whether the
+        passed cluster should be ignored for further
+        processing.
 
         :param: cluster Cluster to test.
         :return: Boolean indicating whether the cluster should be ignored.
@@ -55,6 +75,5 @@ class AbstractAnalyser:
         Processes the defined cluster.
 
         :param cluster: Cluster to process.
-        :return:
         """
         raise NotImplementedError
